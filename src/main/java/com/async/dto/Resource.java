537ac946415d4717d315f21a1006ab639aa1fb32
Nguyen Vu Nhan <nhannv3@vng.com.vn>
2022-02-09 13:59:27 +0700

    Refactor Resource

--- src/main/java/com/async/dto/Resource.java
+++ src/main/java/com/async/dto/Resource.java
@@ -14,29 +14,29 @@ public class Resource {
     private String id;
 
     private String name;
-//
-//    @JsonProperty("parent_fxf")
-//    private List<String> parentFxf;
-//
-//    private String description;
-//
-//    private String attribution;
-//
-//    private String type;
-//
-//    private String updatedAt;
-//
-//    private String createdAt;
-//
-//    @JsonProperty("columns_name")
-//    private List<String> columnsName;
-//
-//    @JsonProperty("columns_field_name")
-//    private List<String> columnsFieldName;
-//
-//    @JsonProperty("columns_datatype")
-//    private List<String> columnsDatatype;
-//
-//    @JsonProperty("columns_description")
-//    private List<String> columnsDescription;
+
+    @JsonProperty("parent_fxf")
+    private List<String> parentFxf;
+
+    private String description;
+
+    private String attribution;
+
+    private String type;
+
+    private String updatedAt;
+
+    private String createdAt;
+
+    @JsonProperty("columns_name")
+    private List<String> columnsName;
+
+    @JsonProperty("columns_field_name")
+    private List<String> columnsFieldName;
+
+    @JsonProperty("columns_datatype")
+    private List<String> columnsDatatype;
+
+    @JsonProperty("columns_description")
+    private List<String> columnsDescription;
 }
